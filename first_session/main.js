b55f305abcaf54f5da41334ce2cb030fe7f978d7
Julia Szele-Nemeth <julia.szelenemeth@gmail.com>
2016-05-07 15:44:18 -0700

    Commit the real app what we did together

--- first_session/main.js
+++ first_session/main.js
@@ -2,31 +2,27 @@ function guesser(n) { //3
     var upperBound = 5;
     var lowerBound = 0;
     var result= guessMyNumber(n);
-    var max = 10; 
-    while (result !== 0 && max > 0) { // while (result !== 0)
-        console.log("result : "+result);
+    while (result !== 0) { // while (result !== 0)
         if (result === -1) {
           // n is bigger than the random
           var newGuess = Math.floor((n - lowerBound) / 2); //2
           upperBound = n; 
           n = newGuess;
         } else if (result === 1) {
-          // n is smaller than the random
-          var newGuess = Math.ceil((upperBound + n) / 2); //2
+          // n is smalljser than the random
+          var newGuess = Math.floor((upperBound - n) / 2); //2
           lowerBound = n; 
           n = newGuess;
         } else {
           return "The number is not in range";
         }
         result= guessMyNumber(n);
-        max --;
     }
     return "We found the number! " + n;
 }
 
 var random = randInt(5); //5
 function guessMyNumber(n) { // 2
-  console.log(n);
   if (n > 5) {
     return undefined;
   } else if (n === random) {
